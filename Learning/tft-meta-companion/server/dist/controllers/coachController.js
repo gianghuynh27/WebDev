0836ec2f409c3e86bf99aa7c8802b1b832d736c9
Giang(Gin) Huynh <hhgiang2000@gmail.com>
2026-06-03 20:48:33 -0500

    add gitignore

--- Learning/tft-meta-companion/server/dist/controllers/coachController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCoachRecommendationController = getCoachRecommendationController;
-const coachService_1 = require("../services/coachService");
-function getCoachRecommendationController(req, res) {
-    const recommendation = (0, coachService_1.getCoachRecommendation)(req.body);
-    res.json(recommendation);
-}
--- Learning/tft-meta-companion/server/dist/routes/coachRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const coachController_1 = require("../controllers/coachController");
-const router = (0, express_1.Router)();
-router.post("/", coachController_1.getCoachRecommendationController);
-exports.default = router;
